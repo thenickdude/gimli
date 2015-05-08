2155d0a02f70e0392b12356a84f5e479327d737b
Nicholas Sherlock <n.sherlock@gmail.com>
2015-05-08 16:24:40 +1200

    Enable no_intra_emphasis for Markdown files to support GitHub dialect

--- ext/github_markup.rb
+++ ext/github_markup.rb
@@ -5,7 +5,7 @@ module GitHub::Markup
   alias_method :old_render, :render
   def render(filename, content = nil)
     if Regexp.compile("\\.(md|mkdn?|mdwn|mdown|markdown|litcoffee)$") =~ filename
-      Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :tables => true).render(content)
+      Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :tables => true, :no_intra_emphasis => true).render(content)
     else
       old_render(filename, content)
     end
